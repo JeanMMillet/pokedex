21f6fb1519214156a88212e9b73122b892f5e6ea
JeanMMillet <millet.jm@live.com>
2023-03-29 12:41:01 +0200

    Add pokemon and buttons to change between them

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@
 import './App.css'
 import MyTitle from './components/MyTitle';
 import PropTypes from "prop-types";
+import { useState } from 'react';
 // import PokemonCard from './components/PokemonCard';
 
 
 function App() {
   const pokemonList = [
-
     {
       name: "bulbasaur",
       imgSrc:
@@ -15,14 +15,55 @@ function App() {
     },
 
     {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+
+    },
+
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+
+    },
+
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+
+    },
+
+    {
       name: "mew",
     },
+
   ];
 
-  return (
+  const [pokemonIndex, setPokemonIndex] = useState(0)
 
+  const handleClickNext = () => {
+    if (pokemonIndex < pokemonList.length - 1)
+      setPokemonIndex(pokemonIndex + 1)
+  }
+  const handleClickPrecedent = () => {
+    if (pokemonIndex > 0) {
+      setPokemonIndex(pokemonIndex - 1)
+    }
+
+  }
+
+  return (
     <div>
-      <PokemonCard pokemon={pokemonList[0]} />
+      <div>
+        <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+      </div>
+      <div>
+        <button onClick={handleClickPrecedent}>Précédent</button>
+        <button onClick={handleClickNext}>Suivant</button>
+
+      </div>
     </div>
   );
 }
