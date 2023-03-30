8a704c0ccd80c769c0f47087ebf2b6de2e4c03af
JeanMMillet <millet.jm@live.com>
2023-03-30 17:05:30 +0200

    add components for buttons and put them in a component Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -18,58 +18,40 @@ function App() {
       name: "charmander",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
-
     },
 
     {
       name: "squirtle",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
-
     },
 
     {
       name: "pikachu",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-
     },
 
     {
       name: "mew",
     },
-
   ];
 
   const [pokemonIndex, setPokemonIndex] = useState(0)
 
-  const handleClickNext = () => {
-    if (pokemonIndex < pokemonList.length - 1)
-      setPokemonIndex(pokemonIndex + 1)
-  }
-  const handleClickPrecedent = () => {
-    if (pokemonIndex > 0) {
-      setPokemonIndex(pokemonIndex - 1)
-    }
-
-  }
 
   return (
     <div>
-      <div>
-        <PokemonCard pokemon={pokemonList[pokemonIndex]} />
-      </div>
-      <div>
-        <button onClick={handleClickPrecedent}>Précédent</button>
-        <button onClick={handleClickNext}>Suivant</button>
 
-      </div>
+      <NavBar index={pokemonIndex} setter={setPokemonIndex} list={pokemonList} />
+      <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+
     </div>
   );
 }
 
 function PokemonCard(props) {
-  return <figure>
+  return <figure className='myImg' >
     <img src={props.pokemon.imgSrc} alt="???" />
     <figcaption>{props.pokemon.name}</figcaption>
   </figure>
@@ -81,5 +63,42 @@ PokemonCard.propTypes = {
     src: PropTypes.string
   })
 }
+
+function NavBar(props) {
+  const { index, setter, list } = props
+
+  return (
+    <>
+      <Precedent precedent={index > 0 ? true : false} setter={setter} index={index} />
+      <Next next={index < list.length - 1 ? true : false} setter={setter} index={index} />
+    </>
+
+
+  )
+}
+
+
+
+
+function Next(props) {
+  const { index, setter, next } = props
+  const handleClickNext = () => {
+    setter(index + 1)
+  }
+  if (next) {
+    return <button onClick={handleClickNext}>Suivant</button>
+  }
+}
+
+function Precedent(props) {
+  const { index, setter, precedent } = props
+  const handleClickPrecedent = () => {
+    setter(index - 1)
+  }
+  if (precedent) {
+    return <button onClick={handleClickPrecedent}>Précédent</button>
+  }
+}
+
 
 export default App
